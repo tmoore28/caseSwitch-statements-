dd3e6961feb24870b518c0813e2a3030347eaea0
tmoore28 <mooretylor28@gmail.com>
2021-11-28 20:59:11 -0500

    none of my files are tracking to git hub have to track again for some reason

---
+++ switch case statement/switch case statement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace switch_case_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            var response = Console.ReadLine();
+            switch (response)
+            {
+                case "math":
+                    Console.WriteLine($"Math is cool!");
+                    break;
+                case "english":
+                    Console.WriteLine($"Thats ok, but i dont like to read");
+                    break;
+                case "science":
+                    Console.WriteLine($"Science is alot of memorization and studying!");
+                    break;
+                case "spanish":
+                    Console.WriteLine($"I dont know anything about that i took French");
+                    break;
+                case "history":
+                    Console.WriteLine($"History is just stories of the past!");
+                    break;
+                default:
+                    Console.WriteLine($"Thats interesting i havnt heard of that on yet! Whats that the study of?");
+                    break;
+            }
+        }
+    }
+}
